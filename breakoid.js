15ebe5b1bb75c6d57df802ae35ae0f2ee847a8b8
thibaultCha <thibaultcha@me.com>
2013-03-18 18:34:36 +0100

    simplified ball code. fix ball hit top bound. Brick width it automatically calculated.

--- breakoid.js
+++ breakoid.js
@@ -3,27 +3,27 @@
 ;(function (window) {
 
     function Breakoid (canvasId) {
-        Breakoid.NBROWS       = 7
-        Breakoid.NBCOLS       = 10
-        Breakoid.BRICK_WIDTH  = 48
-        Breakoid.BRICK_HEIGHT = 15
-        Breakoid.EMPTY_SPACE  = 5
-        Breakoid.BAR_WIDTH    = 80
-        Breakoid.BAR_HEIGHT   = 10
-        Breakoid.BAR_COLOR    = '#333333'
-        Breakoid.BAR_MOVE     = 35
-        Breakoid.BALL_COLOR   = 'red'
-        Breakoid.BALL_SIZE    = 80
-        Breakoid.BALL_SPEED   = 2
 
+        Breakoid.TICKS_INTERVAL = 30
+        Breakoid.NBROWS         = 1
+        Breakoid.NBCOLS         = 10
+        Breakoid.BRICK_HEIGHT   = 15
+        Breakoid.EMPTY_SPACE    = 5
+        Breakoid.BAR_WIDTH      = 80
+        Breakoid.BAR_HEIGHT     = 10
+        Breakoid.BAR_COLOR      = '#333333'
+        Breakoid.BAR_MOVE       = 35
+        Breakoid.BALL_COLOR     = '#222222'
+        Breakoid.BALL_SIZE      = 8
 
         var _bricksArray = new Array(Breakoid.NBROWS)
         , _gameWidth, _gameHeight // canvas width/height
+        , _brickWidth
         , _ctx // context
         , _barX, _barY // bar position
-        , _ballX = 100, _ballY = 250, _ballDirX = 1, _ballDirY = -1 // ball vars
-        , _interval, _speed = 10 // interval between ticks and speed of ticks
-        , _won = false, _pause = false // state of the game
+        , _ballX = 100, _ballY = 250, _ballDirX = 1, _ballDirY = -1, _ballSpeed = 5 // ball vars
+        , _interval // interval
+        , _won, _pause = false // state of the game
 
         function __construct (canvasId) {
             var canvas = document.getElementById(canvasId)
@@ -31,9 +31,9 @@
                 _ctx        = canvas.getContext('2d')
                 _gameWidth  = canvas.width
                 _gameHeight = canvas.height
+                _brickWidth = (_gameWidth / Breakoid.NBCOLS) - (Breakoid.EMPTY_SPACE * Breakoid.NBCOLS / Breakoid.NBCOLS+0.5)
                 _barX       = (canvas.width / 2) - (Breakoid.BAR_WIDTH / 2)
                 _barY       = canvas.height - Breakoid.BAR_HEIGHT
-
                 buildGame()
             } else {
                 console.log('No canvas with id: %s', canvasId)
@@ -48,7 +48,7 @@
                 _ctx.fillStyle = "rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")"
                 _bricksArray[i].color = _ctx.fillStyle
                 for (var j=0 ; j<Breakoid.NBCOLS ; j++) {
-                    _ctx.fillRect( (j * (Breakoid.BRICK_WIDTH+Breakoid.EMPTY_SPACE)), (i * (Breakoid.BRICK_HEIGHT+Breakoid.EMPTY_SPACE)), Breakoid.BRICK_WIDTH, Breakoid.BRICK_HEIGHT)
+                    _ctx.fillRect( (j * (_brickWidth + Breakoid.EMPTY_SPACE)) + Breakoid.EMPTY_SPACE, (i * (Breakoid.BRICK_HEIGHT + Breakoid.EMPTY_SPACE)) + Breakoid.EMPTY_SPACE, _brickWidth, Breakoid.BRICK_HEIGHT)
                     _bricksArray[i][j] = true // a brick is present
                 }
             }
@@ -59,80 +59,83 @@
             window.document.onkeydown = keyboardListener
         };
 
+        function keyboardListener (e) {
+            switch (e.keyCode) {
+                case 39: // right
+                    if (_barX + Breakoid.BAR_MOVE + Breakoid.BAR_WIDTH <= _gameWidth) _barX += Breakoid.BAR_MOVE
+                break
+                case 37: // left
+                    if (_barX - Breakoid.BAR_MOVE >= 0) _barX -= Breakoid.BAR_MOVE
+                break
+                case 80: // pause
+                    if (!_pause)
+                        pause()
+                    else
+                        resume()
+                break
+            }
+        };
+
         function tick () {
             clearContext()
             // Ball
-            // right
-            if (((_ballX + _ballDirX * Breakoid.BALL_SPEED) + Breakoid.BALL_SIZE) > _gameWidth) _ballDirX = -1 
-            // left
-            else if ((_ballX + _ballDirX * Breakoid.BALL_SPEED) < 0 + Breakoid.BALL_SIZE) _ballDirX =  1 
-            // game is lost
-            if (((_ballY + _ballDirY * Breakoid.BALL_SPEED) + Breakoid.BALL_SIZE) > _gameHeight) _ballDirY = -1
-            else {
-                // top
-                if ((_ballY + _ballDirY * Breakoid.BALL_SPEED) < 0 + Breakoid.BALL_SIZE) _ballDirY =  1
-                else {
-                    // Ball touches the bar
-                    if ( ((_ballY + _ballDirY * Breakoid.BALL_SPEED) > (_gameHeight - Breakoid.BAR_HEIGHT)) && ((_ballX + _ballDirX * Breakoid.BALL_SPEED) >= _barX) && ((_ballX + _ballDirX * Breakoid.BALL_SPEED) <= (_ballX + Breakoid.BAR_WIDTH)) ) {
-                        _ballDirY = -1
-                        //_ballDirX = 2 * (_ballX - (_barX + Breakoid.BAR_WIDTH / 2)) / _gameWidth
-                    }
-                }
-            }
-
-            // Ball in brick zone
-            if ( (_ballY - Breakoid.BALL_SIZE) <= Breakoid.NBROWS * (Breakoid.BRICK_HEIGHT + Breakoid.EMPTY_SPACE) ) {
-                var Y = Math.floor((_ballY - Breakoid.BALL_SIZE + Breakoid.BRICK_HEIGHT) / (Breakoid.BRICK_HEIGHT + Breakoid.EMPTY_SPACE)) - 1
-                var X = Math.floor(_ballX / (Breakoid.BRICK_WIDTH + Breakoid.EMPTY_SPACE))
-                if (_bricksArray[Y][X]) {
-                    _bricksArray[Y][X] = false
-                    _ballDirY = 1
-                }
-            }
-
-            _ballX += _ballDirX * Breakoid.BALL_SPEED
-            _ballY += _ballDirY * Breakoid.BALL_SPEED
-
-            _ctx.fillStyle = Breakoid.BALL_COLOR
-            _ctx.beginPath()
-            _ctx.arc(_ballX, _ballY, Breakoid.BALL_SIZE, 0, Math.PI * 2, true)
-            _ctx.closePath()
-            _ctx.fill()
+            ballOnTick()
             // Bricks
             _won = true
             for (var i=0 ; i<Breakoid.NBROWS ; i++) {
                 _ctx.fillStyle = _bricksArray[i].color
                 for (var j=0 ; j<Breakoid.NBCOLS ; j++) {
                     if (_bricksArray[i][j]) {
-                        _ctx.fillRect( (j * (Breakoid.BRICK_WIDTH+Breakoid.EMPTY_SPACE)), (i * (Breakoid.BRICK_HEIGHT+Breakoid.EMPTY_SPACE)), Breakoid.BRICK_WIDTH, Breakoid.BRICK_HEIGHT)
+                        _ctx.fillRect( (j * (_brickWidth+Breakoid.EMPTY_SPACE)) + Breakoid.EMPTY_SPACE, (i * (Breakoid.BRICK_HEIGHT+Breakoid.EMPTY_SPACE)) + Breakoid.EMPTY_SPACE, _brickWidth, Breakoid.BRICK_HEIGHT)
                         _won = false
                     }
                 }
             }
-            if (_won)
-                gameWon()
+            //if (_won)
+                //gameWon()
             // Bar
             _ctx.fillStyle = Breakoid.BAR_COLOR
             _ctx.fillRect(_barX, _barY, Breakoid.BAR_WIDTH, Breakoid.BAR_HEIGHT)
         };
 
-        function keyboardListener (e) {
-            switch (e.keyCode) {
-                case 39: // right
-                    if ( (_barX + Breakoid.BAR_MOVE + Breakoid.BAR_WIDTH) <= _gameWidth ) _barX += Breakoid.BAR_MOVE
-                break;
-
-                case 37: // left
-                    if ( ((_barX - Breakoid.BAR_MOVE)) >= 0 ) _barX -= Breakoid.BAR_MOVE
-                break;
-            
-                case 80:
-                    if (!_pause)
-                        pause()
-                    else
-                        resume()
+        function ballOnTick () {
+            var nextX = _ballX + _ballDirX * _ballSpeed
+            var nextY = _ballY + _ballDirY * _ballSpeed
+            if (nextX + Breakoid.BALL_SIZE > _gameWidth) _ballDirX  = -1 // right border
+            else if (nextX < 0 + Breakoid.BALL_SIZE)     _ballDirX  = 1 // left border
+            if (nextY + Breakoid.BALL_SIZE > _gameHeight) _ballDirY = -1 // game is lost gameLost()
+            else {
+                if (nextY < 0 + Breakoid.BALL_SIZE) _ballDirY = 1 // top border
+                else {
+                    // Ball touches the bar
+                    if ( 
+                        nextY > _gameHeight - Breakoid.BAR_HEIGHT - Breakoid.BALL_SIZE
+                        && (nextX >= _barX && nextX <= _barX + Breakoid.BAR_WIDTH)
+                        ) {
+                        _ballDirY = -1
+                        //_ballDirX = 2 * (_ballX - (_barX + Breakoid.BAR_WIDTH / 2)) / _gameWidth
+                    }
+                }
             }
-        };
+            
+            if ( _ballY - Breakoid.BALL_SIZE <= Breakoid.NBROWS * (Breakoid.BRICK_HEIGHT + Breakoid.EMPTY_SPACE) // ball is in brick zone
+                && _ballY - Breakoid.BALL_SIZE > Breakoid.BALL_SIZE/2) { // ball touched the top border: no need to enter here.
+                var Y = Math.floor((_ballY - Breakoid.BALL_SIZE + Breakoid.BRICK_HEIGHT) / (Breakoid.BRICK_HEIGHT + Breakoid.EMPTY_SPACE)) - 1
+                var X = Math.floor(_ballX / (_brickWidth + Breakoid.EMPTY_SPACE))
+                if (_bricksArray[Y][X]) {
+                    _bricksArray[Y][X] = false
+                    _ballDirY = 1
+                }
+            }
+
+            _ballX += _ballDirX * _ballSpeed
+            _ballY += _ballDirY * _ballSpeed
+            _ctx.fillStyle = Breakoid.BALL_COLOR
+            _ctx.beginPath()
+            _ctx.arc(_ballX, _ballY, Breakoid.BALL_SIZE, 0, Math.PI * 2, true)
+            _ctx.closePath()
+            _ctx.fill()
+        }
 
         function clearContext () {
             _ctx.clearRect(0, 0, _gameWidth, _gameHeight)
@@ -155,14 +158,14 @@
 
         function resume () {
             _pause = false
-            _interval = setInterval(tick, _speed)
+            _interval = setInterval(tick, Breakoid.TICKS_INTERVAL)
         };
 
         /* PUBLIC METHODS */
 
         this.start = function () {
             if (_ctx)
-                _interval = setInterval(tick, _speed)
+                _interval = setInterval(tick, Breakoid.TICKS_INTERVAL)
             else
                 console.log('Error during initialization. Start aborted.')
         };
