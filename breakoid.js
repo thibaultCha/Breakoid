8112691ef59c14a796b84c770f0ef6e6a124aac0
thibaultCha <thibaultcha@me.com>
2013-03-18 19:24:11 +0100

    bar moves to borders. improvements.

--- breakoid.js
+++ breakoid.js
@@ -4,7 +4,7 @@
 
     function Breakoid (canvasId) {
 
-        Breakoid.TICKS_INTERVAL = 30
+        Breakoid.TICKS_INTERVAL = 10
         Breakoid.NBROWS         = 1
         Breakoid.NBCOLS         = 10
         Breakoid.BRICK_HEIGHT   = 15
@@ -12,7 +12,7 @@
         Breakoid.BAR_WIDTH      = 80
         Breakoid.BAR_HEIGHT     = 10
         Breakoid.BAR_COLOR      = '#333333'
-        Breakoid.BAR_MOVE       = 35
+        Breakoid.BAR_MOVE       = 40
         Breakoid.BALL_COLOR     = '#222222'
         Breakoid.BALL_SIZE      = 8
 
@@ -21,7 +21,7 @@
         , _brickWidth
         , _ctx // context
         , _barX, _barY // bar position
-        , _ballX = 100, _ballY = 250, _ballDirX = 1, _ballDirY = -1, _ballSpeed = 5 // ball vars
+        , _ballX = 100, _ballY = 150, _ballDirX = 1, _ballDirY = -1, _ballSpeed = 1 // ball vars //TODO PLACE BALL AUTO
         , _interval // interval
         , _won, _pause = false // state of the game
 
@@ -62,10 +62,16 @@
         function keyboardListener (e) {
             switch (e.keyCode) {
                 case 39: // right
-                    if (_barX + Breakoid.BAR_MOVE + Breakoid.BAR_WIDTH <= _gameWidth) _barX += Breakoid.BAR_MOVE
+                    if (_barX + Breakoid.BAR_MOVE + Breakoid.BAR_WIDTH <= _gameWidth)
+                        _barX += Breakoid.BAR_MOVE
+                    else
+                        _barX = _gameWidth - Breakoid.BAR_WIDTH
                 break
                 case 37: // left
-                    if (_barX - Breakoid.BAR_MOVE >= 0) _barX -= Breakoid.BAR_MOVE
+                    if (_barX - Breakoid.BAR_MOVE >= 0)
+                        _barX -= Breakoid.BAR_MOVE
+                    else
+                        _barX = 0
                 break
                 case 80: // pause
                     if (!_pause)
@@ -78,7 +84,6 @@
 
         function tick () {
             clearContext()
-            // Ball
             ballOnTick()
             // Bricks
             _won = true
@@ -99,18 +104,18 @@
         };
 
         function ballOnTick () {
-            var nextX = _ballX + _ballDirX * _ballSpeed
-            var nextY = _ballY + _ballDirY * _ballSpeed
-            if (nextX + Breakoid.BALL_SIZE > _gameWidth) _ballDirX  = -1 // right border
-            else if (nextX < 0 + Breakoid.BALL_SIZE)     _ballDirX  = 1 // left border
-            if (nextY + Breakoid.BALL_SIZE > _gameHeight) _ballDirY = -1 // game is lost gameLost()
+            _ballX += _ballDirX * _ballSpeed
+            _ballY += _ballDirY * _ballSpeed
+            if (_ballX + Breakoid.BALL_SIZE > _gameWidth) _ballDirX  = -1 // right border
+            else if (_ballX < 0 + Breakoid.BALL_SIZE)     _ballDirX  = 1 // left border
+            if (_ballY + Breakoid.BALL_SIZE > _gameHeight) _ballDirY = -1 // game is lost gameLost()
             else {
-                if (nextY < 0 + Breakoid.BALL_SIZE) _ballDirY = 1 // top border
+                if (_ballY < 0 + Breakoid.BALL_SIZE) _ballDirY = 1 // top border
                 else {
                     // Ball touches the bar
                     if ( 
-                        nextY > _gameHeight - Breakoid.BAR_HEIGHT - Breakoid.BALL_SIZE
-                        && (nextX >= _barX && nextX <= _barX + Breakoid.BAR_WIDTH)
+                        _ballY > _gameHeight - Breakoid.BAR_HEIGHT - Breakoid.BALL_SIZE
+                        && (_ballX >= _barX && _ballX <= _barX + Breakoid.BAR_WIDTH)
                         ) {
                         _ballDirY = -1
                         //_ballDirX = 2 * (_ballX - (_barX + Breakoid.BAR_WIDTH / 2)) / _gameWidth
@@ -118,7 +123,7 @@
                 }
             }
             
-            if ( _ballY - Breakoid.BALL_SIZE <= Breakoid.NBROWS * (Breakoid.BRICK_HEIGHT + Breakoid.EMPTY_SPACE) // ball is in brick zone
+            if (_ballY - Breakoid.BALL_SIZE <= Breakoid.NBROWS * (Breakoid.BRICK_HEIGHT + Breakoid.EMPTY_SPACE) // ball is in brick zone
                 && _ballY - Breakoid.BALL_SIZE > Breakoid.BALL_SIZE/2) { // ball touched the top border: no need to enter here.
                 var Y = Math.floor((_ballY - Breakoid.BALL_SIZE + Breakoid.BRICK_HEIGHT) / (Breakoid.BRICK_HEIGHT + Breakoid.EMPTY_SPACE)) - 1
                 var X = Math.floor(_ballX / (_brickWidth + Breakoid.EMPTY_SPACE))
@@ -128,14 +133,12 @@
                 }
             }
 
-            _ballX += _ballDirX * _ballSpeed
-            _ballY += _ballDirY * _ballSpeed
             _ctx.fillStyle = Breakoid.BALL_COLOR
             _ctx.beginPath()
             _ctx.arc(_ballX, _ballY, Breakoid.BALL_SIZE, 0, Math.PI * 2, true)
             _ctx.closePath()
             _ctx.fill()
-        }
+        };
 
         function clearContext () {
             _ctx.clearRect(0, 0, _gameWidth, _gameHeight)
