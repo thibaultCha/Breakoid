16352396fdb9b234e81d8fb49e4786343ced812e
thibaultCha <thibaultcha@me.com>
2013-03-24 15:56:25 +0100

    State of the game variables to restart game.

--- breakoid.js
+++ breakoid.js
@@ -5,8 +5,8 @@
 
     function Breakoid (canvasId) {
 
-        Breakoid.TICKS_INTERVAL = 5
-        Breakoid.NBROWS         = 6
+        Breakoid.TICKS_INTERVAL = 1
+        Breakoid.NBROWS         = 1
         Breakoid.NBCOLS         = 10
         Breakoid.BRICK_HEIGHT   = 15
         Breakoid.EMPTY_SPACE    = 5
@@ -24,11 +24,11 @@
         , _barX, _barY, _barWidth, _barVelocity = 0 // bar properties
         , _ballX, _ballY, _ballDirX = -1, _ballDirY = 1 // ball properties
         , _interval // interval
-        , _won, _pause = false // state of the game
+        , _running = false, _pause = false // state of the game
         , _keys = [] 
 
         /**
-        * Class constructor from canvas' id. Will calculate all element's positions.
+        * Class constructor from canvas' id.
         *
         * @method __construct
         * @param {string} canvasId The html canvas's id 
@@ -42,10 +42,6 @@
                 _gameHeight = canvas.height
                 _barWidth   = _gameWidth / 8.0
                 _brickWidth = (_gameWidth / Breakoid.NBCOLS) - (Breakoid.EMPTY_SPACE * Breakoid.NBCOLS / Breakoid.NBCOLS+0.5)
-                _barX       = (canvas.width / 2) - (_barWidth / 2)
-                _barY       = canvas.height - Breakoid.BAR_HEIGHT - 2
-                _ballX      = _gameWidth / 1.3
-                _ballY      = _gameHeight / 2
                 buildGame()
             } else {
                 console.log('No canvas with id: %s', canvasId)
@@ -53,13 +49,19 @@
         }  __construct(canvasId);
 
         /**
-        * Build bricks and bar assuming that all elements' positions have been calculated.
+        * Calculate all elements positions and build the first frame.
         * Bind the keyboard for shortcuts and bar moves.
         *
         * @method buildGame
         * @return {void}
         */
         function buildGame () {
+            console.log(this)
+            _barX       = (_gameWidth / 2) - (_barWidth / 2)
+            _barY       = _gameHeight - Breakoid.BAR_HEIGHT - 2
+            _ballX      = _gameWidth / 1.3
+            _ballY      = _gameHeight / 2
+            clearContext()
             // Bricks
             for (var i=0 ; i<Breakoid.NBROWS ; i++) {
                 _bricksArray[i] = new Array(Breakoid.NCOLS)
@@ -81,6 +83,10 @@
                     if (!_pause) pause()
                     else resume()
                 } 
+                else if (e.keyCode == 82) {
+                    if (!_running)
+                        Breakoid.start()
+                }
             }
             window.document.onkeyup = function (e) {
                 _keys[e.keyCode] = false
@@ -98,17 +104,17 @@
             clearContext()
             ballOnTick() // Ball
             // Bricks
-            _won = true
+            var won = true
             for (var i=0 ; i<Breakoid.NBROWS ; i++) {
                 _ctx.fillStyle = _bricksArray[i].color
                 for (var j=0 ; j<Breakoid.NBCOLS ; j++) {
                     if (_bricksArray[i][j]) {
                         _ctx.fillRect( (j * (_brickWidth+Breakoid.EMPTY_SPACE)) + Breakoid.EMPTY_SPACE, (i * (Breakoid.BRICK_HEIGHT+Breakoid.EMPTY_SPACE)) + Breakoid.EMPTY_SPACE, _brickWidth, Breakoid.BRICK_HEIGHT)
-                        _won = false
+                        won = false
                     }
                 }
             }
-            //if (_won)
+            //if (won)
                 //gameWon()
             barOnTick() // Bar
         };
@@ -124,7 +130,7 @@
             _ballY += _ballDirY
             if (_ballX + Breakoid.BALL_SIZE > _gameWidth) _ballDirX  = -1 // right border
             else if (_ballX - Breakoid.BALL_SIZE < 0 )    _ballDirX  = 1 // left border
-            if (_ballY + Breakoid.BALL_SIZE > _gameHeight) _ballDirY = -1
+            if (_ballY + Breakoid.BALL_SIZE > _gameHeight) gameLost()
             else {
                 if (_ballY - Breakoid.BALL_SIZE < 0 ) _ballDirY = 1 // top border
                 else {
@@ -202,8 +208,10 @@
         * @return {void}
         */
         function gameWon () {
+            _running = false
             clearInterval(_interval)
-            alert('You win')
+            alert('You win. "R" to restart.')
+            buildGame()
         };
 
         /**
@@ -213,8 +221,10 @@
         * @return {void}
         */
         function gameLost () {
+            _running = false
             clearInterval(_interval)
-            alert('You lost')
+            alert('You lost. "R" to restart.')
+            buildGame()
         };
 
         /**
@@ -248,8 +258,10 @@
         * @return {void}
         */
         this.start = function () {
-            if (_ctx)
+            if (_ctx && !_running) {
+                _running = true
                 _interval = setInterval(tick, Breakoid.TICKS_INTERVAL)
+            }
             else
                 console.log('Error during initialization. Start aborted.')
         };
